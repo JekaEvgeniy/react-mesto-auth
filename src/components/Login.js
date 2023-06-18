6c58be6efbb772e60293de9f5f30b2722053e8b5
JekaEvgeniy <pruven@yandex.ru>
2023-06-18 17:35:01 +0300

    feat: import section login/register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,13 @@
 function Login() {
 	return (
-		<h2>Login.js</h2>
+		<section className="login">
+			<h2 className="login__header">Вход</h2>
+			<form action="#" className="login__form">
+				<input className="login__input" type="email" name="email" placeholder="Email" required />
+				<input className="login__input" type="password" name="password" placeholder="Пароль" required />
+				<button className="login__button" type="submit">Войти</button>
+			</form>
+		</section>
 	)
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,15 @@
 function Register(){
 	return (
-		<h2>Register.js</h2>
+		<section className="login">
+			<h2 className="login__header">Регистрация</h2>
+			<form action="#" className="login__form">
+				<input className="login__input" type="email" name="email" placeholder="Email" required />
+				<input className="login__input" type="password" name="password" placeholder="Пароль" required />
+				<button className="login__button" type="submit">Зарегистрироваться</button>
+			</form>
+
+			<p className="login__caption">Уже зарегистрированы? <a className="login__link" href="#">Войти</a></p>
+		</section>
 	)
 }
 
