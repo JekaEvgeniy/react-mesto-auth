9e000364228454f96da76caac949fbc0e8fd5808
JekaEvgeniy <pruven@yandex.ru>
2023-06-18 17:35:26 +0300

    fix: remove comments login/register

--- src/components/Main.js
+++ src/components/Main.js
@@ -53,27 +53,3 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace, cards, onCardLike, onCa
 }
 
 export default Main;
-
-
-{/*
-			<section className="login">
-				<h2 className="login__header">Регистрация</h2>
-				<form action="#" className="login__form">
-					<input className="login__input" type="email" name="email" placeholder="Email" required />
-					<input className="login__input" type="password" name="password" placeholder="Пароль" required />
-					<button className="login__button" type="submit">Зарегистрироваться</button>
-				</form>
-
-				<p className="login__caption">Уже зарегистрированы? <a className="login__link" href="#">Войти</a></p>
-			</section>
-
-
-			<section className="login">
-				<h2 className="login__header">Вход</h2>
-				<form action="#" className="login__form">
-					<input className="login__input" type="email" name="email" placeholder="Email" required />
-					<input className="login__input" type="password" name="password" placeholder="Пароль" required />
-					<button className="login__button" type="submit">Войти</button>
-				</form>
-			</section>
- */}
