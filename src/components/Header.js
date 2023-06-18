865bbeb65c8b05512c9332c633a673b32a9d2325
JekaEvgeniy <pruven@yandex.ru>
2023-06-18 14:16:34 +0300

    feat: add header-actions

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,14 @@ function Header() {
 	return (
 		<header className="header">
 			<img className="header__logo" src={headerLogo} alt="Логотип" loading="lazy" />
+
+			<div className="header__actions">
+				<p className="header__email">email@mail.com</p>
+				<button className="header__button" type="button">Выйти</button>
+			</div>
+			 <div className="header__actions">
+				<a href="#" className="header__link">Регистрация</a>
+			</div>
 		</header>
 	)
 }
